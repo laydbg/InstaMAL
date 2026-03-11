c55a2990353d567ad44c2e5c7bc1b89816a8eed6
Birger Karlsson <birgerk@kth.se>
2026-03-11 12:48:41 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="instamal",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
